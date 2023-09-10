cb384b855575bdc61f29db919f876bbcfb18ac38
Blue <101784753+blue2n@users.noreply.github.com>
2023-09-10 20:29:00 -0300

    Update conta.py

--- py3.4/conta.py
+++ py3.4/conta.py
@@ -12,11 +12,41 @@ class Conta:
 
     def depositar(self, valor):
         self.__aldo += valor
+
+    def __pode_sacar(self, saldo_disponivel):
+        saldo_conta = self.__saldo + self.__limite
+        return saldo_disponivel <= saldo_conta
     
     def sacar(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print('Saldo insuficiente')
 
     def transferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
-        
+
+    @property   
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def get_titular(self):
+        return self.__titular
+    
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @limite.setter
+    def set_limite(self, limite):
+        self.__limite = limite
+    
+    @staticmethod
+    def codigo_banco():
+        return '001'
+    
+    @staticmethod
+    def codigos_bancos():
+        return {'BB' : '001', 'Caixa' : '104', 'Bradesco' : '273'}
